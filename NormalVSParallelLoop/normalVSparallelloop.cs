80d8c5d8d2f632aad88b52c47fdf6ef6e491035d
Angel <tessio1994@gmail.com>
2019-02-10 16:23:15 +0100

    normal VS parallel loop

--- NormalVSParallelLoop/normalVSparallelloop.cs
+++ NormalVSParallelLoop/normalVSparallelloop.cs
@@ -11,7 +11,7 @@ namespace ExampleForLoop
     {
         static void Main()
         {
-
+	    //creation of the array
             double[] array = new double[200 * 1000 * 1000];
 
             for (int i = 0; i < array.Length; i++)
@@ -19,10 +19,11 @@ namespace ExampleForLoop
 
             for (int i = 0; i < 5; i++)
             {
+		//initialize watch and process in serial
                 Stopwatch sw = Stopwatch.StartNew();
                 Serial(array, 2);
                 Console.WriteLine("Serial: {0:f2} s", sw.Elapsed.TotalSeconds);
-
+		//initialize watch and process in parallel
                 sw = Stopwatch.StartNew();
                 ParallelFor(array, 2);
                 Console.WriteLine("Parallel.For: {0:f2} s", sw.Elapsed.TotalSeconds);
@@ -44,6 +45,7 @@ namespace ExampleForLoop
 
         static void ParallelFor(double[] array, double factor)
         {
+	//First we indicate the source index for the first iteration of the loop. Then how far do we want to go, exclusively. Finally, we define an Action. The action is what will be executed.
             Parallel.For(
                 0, array.Length, i => { array[i] = array[i] * factor; });
         }
