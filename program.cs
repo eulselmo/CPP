30d94114074c2dc97fea3fbaa2afa0036bbde6bf
Angel <tessio1994@gmail.com>
2019-02-11 16:47:13 +0100

    example to show countries, density, ...

--- program.cs
+++ program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 //Añadir el segundo parámetro
 class CoutriesOfTheWorld
 {
@@ -10,25 +11,27 @@ class CoutriesOfTheWorld
     public static void Main()
     {
         String[] args = Environment.GetCommandLineArgs();
-        if (args.Length == 1) {
+        if (args.Length == 1 || args.Length == 2) {
          Console.WriteLine("There are no command line arguments.");
          return;
         }
 
         String  theContinent = args[1];
+        int  density =  Int32.Parse(args[2]);
 
         IEnumerable<Country> Countries = GetCountries();
         Console.WriteLine("The number of countries: {0}", Countries.Count()); 
         Console.WriteLine();  
 
         var result = from c in Countries
-                where c.Continent == theContinent && c.Population / c.Area > 70
+                where c.Continent == theContinent && c.Population / c.Area > density
                 orderby c.Population/c.Area descending
                 select c;
 
         foreach(Country c in result)
         {
-            Console.WriteLine(String.Format("{0, -25} {1, 15:n0} {2, 15:n0} {3, 10:n1}", c.Name, c.Population, c.Area, 1.0*c.Population/c.Area));
+            Console.WriteLine(String.Format("{0, -25} {1, 15:n0} {2, 15:n0} {3, 10:n1} {2, 15:n0}", 
+            	c.Name, c.Population, c.Area, 1.0*c.Population/c.Area));
         }
 
     }
